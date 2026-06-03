42b8de404c17e7a91c045ac10f4487ea551f0665
Hugo PropoOps <propopsp@gmail.com>
2026-06-04 01:12:47 +1000

    Update staff-notifications.js

--- services/staff-notifications.js
+++ services/staff-notifications.js
@@ -1,8 +1,10 @@
 module.exports = {
   sendShiftReminders: async () => {
     console.log('[staff-notifications] sendShiftReminders - stub');
+    return { sent: 0, skipped: 0 };
   },
   sendWeeklyRosterSummaries: async () => {
     console.log('[staff-notifications] sendWeeklyRosterSummaries - stub');
+    return { sent: 0, skipped: 0 };
   }
 };
